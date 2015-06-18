66a870d9e4af973727b406b8f49b6755c4dec7da
Andy Baird <Andy.Baird@icloud.com>
2015-06-18 17:25:14 -0400

    setup view/model/controller/collection for shares(all)

--- src/controllers/shares/shares.js
+++ src/controllers/shares/shares.js
@@ -3,8 +3,11 @@
 var $ = require('jquery');
 var router = require('../../util/router');
 var main = require('../../views/main');
+var ShareCollection = require("../../models/shares/share-collection")
 var SharesView = require('../../views/shares/shares');
 
 router.route('', 'shares', function () {
-  main.show(new SharesView());
+  main.show(new SharesView({
+    model: new ShareCollection()
+  }));
 });
--- src/views/shares/shares.js
+++ src/views/shares/shares.js
@@ -1,7 +1,23 @@
 'use strict';
 
 var Backbone = require('backbone');
+var $ = require('jquery');
+var template = require('../template');
 
 module.exports = Backbone.View.extend({
+  template: template('shares/shares'),
   
+  className: 'shares-container',
+  
+  initialize: function () {
+    this.listenTo(this.model, 'change', this.render);
+    this.render();
+  },
+
+  render: function () {
+    this.$el.html(this.template({ 
+      user: this.model.toJSON()
+    }));
+  }
 });
+
