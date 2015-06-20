100cda1a788ed9bee3c61727aaeb74e4eea27336
Andy Baird <Andy.Baird@icloud.com>
2015-06-20 12:35:52 -0400

    wrote script so login/logout only show when needed

--- src/views/shared/page-header.js
+++ src/views/shared/page-header.js
@@ -3,12 +3,19 @@
 var Backbone = require('backbone');
 var template = require('../template');
 var router = require('../../util/router');
+var $ = require('jquery');
 
 // The page-header view is responsible for managing the page-header component
 // links, current-user info in the nav-bar, etc
+  
+
 module.exports = Backbone.View.extend({
   template: template('shared/page-header'),
-
+  
+  // events: {
+  //   'click .login-btn': 'hideLogin',
+  // },
+  
   el: '.page-header',
 
   initialize: function () {
@@ -20,7 +27,14 @@ module.exports = Backbone.View.extend({
   render: function () {
     this.$el.html(this.template({
       page: this.model.nav.get('page'),
-      currentUser: this.model.currentUser.toJSON()
+      currentUser: this.model.currentUser.toJSON()  
     }));
   },
+  
+  // hideLogin: function () {
+  //       // if ($('.currentUser')[0].contents() = '' )
+  //      $('.login-btn')[0].style.display = "none"; 
+  // }
+  
+  
 });
